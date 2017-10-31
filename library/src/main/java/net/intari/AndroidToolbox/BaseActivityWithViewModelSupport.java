af80a61457f5baf511739f975ffd681e33fec2ed
Dmitriy Kazimirov <dmitriy.kazimirov@viorsan.com>
2017-10-31 14:37:25 +0600

    ViewModel support from https://habrahabr.ru/post/328512/

---
+++ library/src/main/java/net/intari/AndroidToolbox/BaseActivityWithViewModelSupport.java
@@ -0,0 +1,110 @@
+package net.intari.AndroidToolbox;
+
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.annotation.Nullable;
+
+
+import net.intari.CustomLogger.CustomLog;
+
+/**
+ * Created by Dmitriy Kazimirov, e-mail:dmitriy.kazimirov@viorsan.com on 13.05.17.
+ * Base Activity with supports for ModeView restore logic from https://habrahabr.ru/post/328512/
+ * and with regular Base Activity code
+ */
+
+public abstract class BaseActivityWithViewModelSupport<T extends BaseViewModel> extends BaseActivityWithGNSSSupport {
+    public static final String TAG = BaseActivityWithViewModelSupport.class.getSimpleName();
+
+    private static final String DATA = "data"; //Для сохранения данных
+    private T data; //Дженерик, ибо для каждого активити используется своя ViewModel
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        if (savedInstanceState != null)
+            data = savedInstanceState.getParcelable(DATA); //Восстанавливаем данные если они есть
+        else
+            connectData(); //Если нету - подключаем новые
+
+
+        setActivity(); //Привязываем активити для ViewModel (если не используем Dagger)
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (data != null) {
+            CustomLog.d(TAG, "Data saved");
+            outState.putParcelable(DATA, (Parcelable) data);
+        }
+
+    }
+
+    /**
+     * Метод onDestroy будет вызываться при любом повороте экрана, так что нам нужно знать
+     * что мы сами закрываем активити, что бы уничтожить данные.
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        CustomLog.d(TAG, "onDestroy");
+        if (isFinishing())
+            destroyData();
+    }
+
+
+    /**
+     * Этот метод нужен только если вы не используете DI.
+     * А так, это простой способ передать активити для каких-то действий с preferences или DB
+     */
+    private void setActivity() {
+        if (data != null) {
+            if (!data.isSetActivity())
+                data.setActivity(this);
+        }
+    }
+
+
+    /**
+     * Возвращаем данные
+     *
+     * @return возврощает ViewModel, которая прикреплена за конкретной активити
+     */
+    public T getData() {
+        CustomLog.d(TAG, "Returning data "+data.toString());
+        return data;
+    }
+
+    /**
+     * Прикрепляем ViewModel к активити
+     *
+     * @param data
+     */
+    public void setData(T data) {
+        this.data = data;
+    }
+
+
+    /**
+     * Уничтожаем данные, предварительно отписываемся от всех подписок Rx
+     */
+    public void destroyData() {
+        if (data != null) {
+            data.globalDispose();
+            data = null;
+            CustomLog.d(TAG,"Data destroyed");
+        }
+    }
+
+
+    /**
+     * Абстрактный метод, который вызывается, если у нас еще нет сохраненных данных
+     */
+    protected abstract void connectData();
+
+
+
+
+}
---
+++ library/src/main/java/net/intari/AndroidToolbox/BaseViewModel.java
@@ -0,0 +1,52 @@
+package net.intari.AndroidToolbox;
+
+import android.app.Activity;
+import android.databinding.BaseObservable;
+
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+
+/**
+ * Created by Dmitriy Kazimirov, e-mail:dmitriy.kazimirov@viorsan.com on 13.05.17.
+ * idea from https://habrahabr.ru/post/328512/
+ */
+public abstract class BaseViewModel extends BaseObservable {
+
+
+    private CompositeDisposable disposables; //Для удобного управления подписками
+    private Activity activity;
+
+
+    protected BaseViewModel() {
+        disposables = new CompositeDisposable();
+    }
+
+
+    /**
+     * Метод добавления новых подписчиков
+     */
+    protected void newDisposable(Disposable disposable) {
+        disposables.add(disposable);
+    }
+
+    /**
+     * Метод для отписки всех подписок разом
+     */
+    public void globalDispose() {
+        disposables.dispose();
+    }
+
+
+    protected Activity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+    }
+
+    public boolean isSetActivity() {
+        return (activity != null);
+    }
+
+}
