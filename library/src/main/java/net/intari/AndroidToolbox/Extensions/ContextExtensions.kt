907f2465d96c6ac4cc88be3674b7e30485c50682
Dmitriy Kazimirov <dmitriy.kazimirov@viorsan.com>
2017-11-18 09:39:45 +0600

    let's just use Ank

--- library/src/main/java/net/intari/AndroidToolbox/Extensions/ContextExtensions.kt
+++ library/src/main/java/net/intari/AndroidToolbox/Extensions/ContextExtensions.kt
@@ -1,14 +1,17 @@
 package net.intari.AndroidToolbox.Extensions
 
+import android.app.AlarmManager
 import android.content.Context
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+import android.view.LayoutInflater
 import android.widget.Toast
 
 /**
  * Created by Dmitriy Kazimirov, e-mail:dmitriy.kazimirov@viorsan.com on 05.11.2017.
  * Kotlin extensions for common Android classes
+ * Also see Anko
  */
-fun Context.toast(message: CharSequence) =
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-fun Context.toast(message: Int) =
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+
 
